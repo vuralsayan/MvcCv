42a47569c3b68208a6caf81ace4315a0c0fd5426
Vural Sayan <vuralsayan01@gmail.com>
2023-09-22 22:41:14 +0300

    Deneyimler listelendi

---
+++ MvcCv/Controllers/DeneyimController.cs
@@ -0,0 +1,21 @@
+﻿using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class DeneyimController : Controller
+    {
+        // GET: Deneyim
+
+        DeneyimRepository repo = new DeneyimRepository();
+        public ActionResult Index()
+        {
+            var degerler = repo.List();         //DeneyimRepository'deki List() metodunu çağırır.
+            return View(degerler);
+        }
+    }
+}
