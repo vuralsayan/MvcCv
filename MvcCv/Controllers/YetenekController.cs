d517b2f7026a593f3a72de082710e3d2522bd90c
Vural Sayan <vuralsayan01@gmail.com>
2023-09-23 18:54:54 +0300

    Yetenekler repository üzerinden listelendi

---
+++ MvcCv/Controllers/YetenekController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+
+namespace MvcCv.Controllers
+{
+    public class YetenekController : Controller
+    {
+        // GET: Yetenek
+        
+        GenericRepository<TblYeteneklerim> repo = new GenericRepository<TblYeteneklerim>(); // GenericRepository sınıfından bir nesne ürettik.
+        public ActionResult Index()
+        {
+            var yetenekler = repo.List();               // GenericRepository sınıfındaki List() metodunu çağırdık.
+            return View(yetenekler);                    // List() metodundan dönen değeri View'a gönderdik.
+        }
+    }
+}
