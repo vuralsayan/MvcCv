c18ab301a834313c1df409b9ec37f95ceb5c80f6
Vural Sayan <vuralsayan01@gmail.com>
2023-09-24 21:28:13 +0300

    Eğitim listesi ve eğitim eklem tamamlandı

---
+++ MvcCv/Controllers/EgitimController.cs
@@ -0,0 +1,34 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class EgitimController : Controller
+    {
+        // GET: Egitim
+        GenericRepository<TblEgitimlerim> repo = new GenericRepository<TblEgitimlerim>();
+        public ActionResult Index()
+        {
+            var egitim = repo.List();
+            return View(egitim);
+        }
+
+        [HttpGet]
+        public ActionResult EgitimEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EgitimEkle(TblEgitimlerim p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
